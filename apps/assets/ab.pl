ba7b4f0a449fc000a45f70f4aeaa666feae8e144
Sergey Yurzin <jurzin.s@gmail.com>
2016-08-06 15:55:27 +0300

    add script parameters to ab.pl

--- apps/assets/ab.pl
+++ apps/assets/ab.pl
@@ -2,16 +2,30 @@
 
 use strict;
 use warnings FATAL => 'all';
+use Scalar::Util;
+use POSIX;
 use FindBin;
 use lib sprintf('%s/../lib', $FindBin::Bin);
 use Benchmark::Factory;
 
 
-my $benchmark = Benchmark::Factory->create(
-    concurency => [1..20],
-    requests_number => 100_000,
-    uri => 'mod_perl.example.com/',
-    output_file_name => 'mod_perl__max_spare_servers_35.csv',
-);
+my %kwargs = @ARGV;
+if (!@ARGV || !$kwargs{'-u'} || !$kwargs{'-n'} || !Scalar::Util::looks_like_number($kwargs{'-n'})) {
+    printf("Usage: %s -u [URL] -n [REPEATS]\n", $0);
+    exit(0);
+}
 
-$benchmark->run();
+for (1..int($kwargs{'-n'})) {
+    my $domain = [split('/', $kwargs{'-u'})]->[0];
+    my $cdate = POSIX::strftime('%d%m%Y_%H%M%S', localtime(time));
+
+    my $benchmark = Benchmark::Factory->create(
+        concurency => [1..20],
+        requests_number => 100,
+        uri => $kwargs{'-u'},
+        output_file_name => sprintf('%s__%s.csv', $domain, $cdate),
+    );
+
+    $benchmark->run();
+}
+
